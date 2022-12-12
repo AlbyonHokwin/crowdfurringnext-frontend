8864bc232177783879bfc9706393c4e0c91f19cb
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-12 18:20:40 +0100

    potscreen almost finished - need to add style and integrate navigation to payment

--- screens/PotScreen.js
+++ screens/PotScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     StyleSheet,
     Text,
@@ -13,62 +13,34 @@ import {
 } from 'react-native'
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const dummyData = {
-    user: {
-        firstname: 'Camille',
-        lastname: 'HAUSTANT',
-        email: 'test@gmail.com',
-        password: 'azerty123',
-        address: {
-            street: '154 Promenade du Verger',
-            additionnal: '',
-            zipCode: '92130',
-            city: 'Issy-Les-Moulineaux',
-        },
-        token: 'thisisatruetoken',
-        picture: 'picture_testFile.png',
-        association: '',
-        admin: false,
-    },
-    contributors: [],
-    pictures: [
-        require('../assets/bounty_testFile1.jpg'),
-        require('../assets/bounty_testFile2.jpg'),
-        require('../assets/bounty_testFile3.jpg'),
-    ],
-    description: 'This is the description of the most amazing existing on Earth. His name : Bounty ! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit. Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin. Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue. Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat. Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna. Aliquam convallis sollicitudin purus. Praesent aliquam, enim at fermentum mollis, ligula massa adipiscing nisl, ac euismod nibh nisl eu lectus. Fusce vulputate sem at sapien. Vivamus leo. Aliquam euismod libero eu enim. Nulla nec felis sed leo placerat imperdiet. Aenean suscipit nulla in justo. Suspendisse cursus rutrum augue. Nulla tincidunt tincidunt mi. Curabitur iaculis, lorem vel rhoncus faucibus, felis magna fermentum augue, et ultricies lacus lorem varius purus. Curabitur eu amet.',
-    info: {
-        specie: 'Chat',
-        race: 'Baltringue',
-        age: 7,
-        sex: 'male',
-    },
-    socialNetwork: [],
-    compensations: ['Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'Petite balade dans le parc', 'Venir jouer avec lui', 'FIN'],
-    documents: [],
-    isValidate: true,
-    startDate: new Date('10-12-2022'),
-    endDate: new Date('25-12-2022'),
-    targetAmount: 1420,
-    currentAmount: 254,
-    animalName: 'Bounty',
-    isClosed: false,
-    draft: false,
-};
+const BACKEND_URL = 'http://192.168.10.140:3000';
 
 const PotScreen = ({ route, navigation }) => {
+    const [pot, setPot] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
     const [modalIndex, setModalIndex] = useState(0);
 
-    const photos = dummyData.pictures.map((photo, i) => {
+    useEffect(() => {
+        (async () => {
+            const slug = "testSlug";
+            const response = await fetch(`${BACKEND_URL}/pots/${slug}`);
+            const data = await response.json();
+
+            data.result && setPot(data.pot);
+        })();
+    }, []);
+
+    if (!pot) return (<></>);
+
+    const photos = pot.pictures.map((photo, i) => {
         return (
             <TouchableOpacity key={i} style={styles.touchablePhoto} activeOpacity={0.8} onPress={() => pressOnPhoto(i)} >
-                <Image source={photo} style={styles.photo} />
+                <Image source={{ uri: photo }} style={styles.photo} />
             </TouchableOpacity>
         );
     });
 
-    const modalPhotos = dummyData.pictures.map((photo, i) => {
+    const modalPhotos = pot.pictures.map((photo, i) => {
         return (
             <View key={i} style={styles.bigPhotoContainer} >
                 <TouchableOpacity style={styles.arrowButton} activeOpacity={0.8} onPress={() => pressLeft()} >
@@ -84,6 +56,13 @@ const PotScreen = ({ route, navigation }) => {
         );
     })
 
+    const compensations = pot.compensations.map((compensation, i) => {
+        return (
+            <Text key={i} style={styles.compensation}>{compensation}</Text>
+        );
+    });
+
+
     const pressOnPhoto = index => {
         setModalIndex(index);
         setModalVisible(true);
@@ -97,43 +76,53 @@ const PotScreen = ({ route, navigation }) => {
         setModalIndex(Math.min(modalPhotos.length - 1, modalIndex + 1));
     }
 
-    const compensations = dummyData.compensations.map((compensation, i) => {
-        return (
-            <Text key={i} style={styles.compensation}>{compensation}</Text>
-        );
-    });
-
     return (
-        <ScrollView contentContainerStyle={styles.scrollContainer} nestedScrollEnabled={true} >
-            <View style={styles.header}>
-                <Text style={styles.name}>{dummyData.animalName}</Text>
-                <Text style={styles.city}>{dummyData.user.address.city}</Text>
-            </View>
+        <View style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollContainer} nestedScrollEnabled={true} >
+                <View style={styles.header}>
+                    <Text style={styles.name}>{pot.animalName}</Text>
+                    <Text style={styles.city}>{pot.user.address.city}</Text>
+                    <Text style={styles.amounts}>{pot.currentAmount}€ / {pot.targetAmount}€</Text>
+                </View>
 
-            <View style={styles.photos}>
-                <ScrollView horizontal={true} style={styles.photosScroll}>
-                    {photos}
-                </ScrollView>
-            </View>
+                <View style={styles.photos}>
+                    <ScrollView horizontal={true} style={styles.photosScroll}>
+                        {photos}
+                    </ScrollView>
+                </View>
 
-            <View style={styles.info}>
-                <Text style={styles.infoText}>Animal : {dummyData.info.specie}</Text>
-                <Text style={styles.infoText}>Race : {dummyData.info.race}</Text>
-                <Text style={styles.infoText}>Age : {dummyData.info.age}</Text>
-                <Text style={styles.infoText}>Sexe : {dummyData.info.sex}</Text>
-            </View>
+                <View style={styles.info}>
+                    <Text style={styles.infoText}>Animal : {pot.info.specie}</Text>
+                    <Text style={styles.infoText}>Race : {pot.info.race}</Text>
+                    <Text style={styles.infoText}>Age : {pot.info.age}</Text>
+                    <Text style={styles.infoText}>Sexe : {pot.info.sex}</Text>
+                </View>
 
-            <View style={styles.descriptionContainer}>
-                <ScrollView style={styles.scrollDescription} nestedScrollEnabled={true}>
-                    <Text style={styles.description}>{dummyData.description}</Text>
-                </ScrollView>
-            </View>
+                <View style={styles.descriptionContainer}>
+                    <ScrollView style={styles.scrollDescription} nestedScrollEnabled={true}>
+                        <Text style={styles.description}>{pot.description}</Text>
+                    </ScrollView>
+                </View>
 
-            <View style={styles.compensations}>
-                <Text style={styles.compensationTitle}>Contre-parties :</Text>
-                <ScrollView style={styles.scrollCompensations} nestedScrollEnabled={true} >
-                    {compensations}
-                </ScrollView>
+                <View style={styles.compensations}>
+                    <Text style={styles.compensationTitle}>Contre-parties :</Text>
+                    <ScrollView style={styles.scrollCompensations} nestedScrollEnabled={true} >
+                        {compensations}
+                    </ScrollView>
+                </View>
+
+                <View style={styles.voidContainer}></View>
+
+                <View><Text>FIN</Text></View>
+            </ScrollView>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity onPress={() => console.log('----')} style={styles.button} activeOpacity={0.8}>
+                    <Text style={styles.textBackButton}>Retour</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => console.log('++++')} style={styles.button} activeOpacity={0.8}>
+                    <Text style={styles.textGiveButton}>Donner</Text>
+                </TouchableOpacity>
             </View>
 
             <Modal
@@ -152,8 +141,7 @@ const PotScreen = ({ route, navigation }) => {
                 </TouchableOpacity>
             </Modal>
 
-            <View><Text>FIN</Text></View>
-        </ScrollView>
+        </View>
     );
 }
 
@@ -163,12 +151,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: StatusBar.currentHeight + 5,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
     },
     scrollContainer: {
         flexGrow: 1,
-        paddingTop: StatusBar.currentHeight + 5,
         justifyContent: 'flex-start',
         alignItems: 'center',
     },
@@ -176,7 +161,6 @@ const styles = StyleSheet.create({
         width: '80%',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        height: Dimensions.get('screen').height / 20,
     },
     name: {
         fontSize: 30,
@@ -187,9 +171,17 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: 'black',
     },
+    amounts: {
+        fontSize: 30,
+        color: 'black',
+        marginVertical: 5,
+        padding: 10,
+        borderRadius: 20,
+        backgroundColor: '#aaaaaa',
+    },
     photos: {
         width: '100%',
-        marginVertical: 20,
+        marginVertical: 10,
     },
     photosScroll: {},
     photo: {
@@ -272,5 +264,36 @@ const styles = StyleSheet.create({
         height: Dimensions.get('screen').width * 0.75,
         width: Dimensions.get('screen').width * 0.75,
         borderRadius: 10,
+    },
+    voidContainer: {
+        height: 100,
+    },
+    buttonsContainer: {
+        position: 'absolute',
+        width: '100%',
+        bottom: 0,
+        zIndex: 1000,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 25,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#aaaaaa',
+        borderRadius: 10,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textBackButton: {
+        fontWeight: '600',
+        fontSize: 20,
+        color: 'black',
+    },
+    textGiveButton: {
+        fontWeight: '600',
+        fontSize: 30,
+        color: 'black',
     },
 });
