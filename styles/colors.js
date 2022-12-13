e1b75f8e232b6756c51af9eead5ef9c09b120f8a
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-13 15:25:04 +0100

    add background and modal colors

--- styles/colors.js
+++ styles/colors.js
@@ -1,3 +1,5 @@
+export const background = 'white';
+export const backgroundModal = 'rgba(100, 100, 100, 0.7)';
 export const light = "white";
 export const dark = "#434449";
 export const primary = "#1F6F78";
