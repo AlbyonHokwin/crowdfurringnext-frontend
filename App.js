4741bdec1e5db92e489c3b7acd09a5cc58cff8c3
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-13 15:30:21 +0100

    change style and icons of bottom bar - deleted unnecessary declared tabbar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as colors from './styles/colors';
 
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from './reducers/user';
 
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -21,40 +22,31 @@ const store = configureStore({
 
 const Tab = createBottomTabNavigator();
 
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = "";
-
-        if (route.name === "Home") iconName = "home";
-        if (route.name === "Message") iconName = "envelope-o";
-        if (route.name === "Pots") iconName = "paw";
-        if (route.name === "Profile") iconName = "user";
-
-        return <FontAwesome name={iconName} size={size} color={color} />
-      },
-      tabBarActiveTintColor: 'purple',
-      tabBarInactiveTintColor: 'black',
-    })}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Message" component={MessageScreen} />
-      <Tab.Screen name="Pots" component={PotsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-    </Tab.Navigator>
-  )
-}
-
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <TabNavigator>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName = "";
+
+            if (route.name === "Home") iconName = "paw";
+            if (route.name === "Message") iconName = "envelope";
+            if (route.name === "Pots") iconName = "cat";
+            if (route.name === "Profile") iconName = "user";
+
+            return <FontAwesome name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: colors.accent,
+          tabBarInactiveTintColor: colors.shade,
+          tabBarStyle: {backgroundColor: colors.primary},
+          headerShown: false,
+        })}>
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Message" component={MessageScreen} />
           <Tab.Screen name="Pots" component={PotsScreen} />
           <Tab.Screen name="Profile" component={ProfileScreen} />
-        </TabNavigator>
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
