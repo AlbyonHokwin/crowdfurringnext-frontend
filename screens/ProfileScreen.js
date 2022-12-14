de085ce78da06494a9f1fa8eeb4e937a32482201
yiengo <yieng.hu@gmail.com>
2022-12-14 16:33:44 +0100

    page profil

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,25 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from "react-native";
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 export default function ProfileScreen() {
     return (
-        <View style={styles.container}>  
-            <Text>PROFILE</Text>
+        <View style={styles.container}>
+        <View style={styles.gestion}>
+         <View style={styles.menu} >
+            <View style={styles.title}>
+            <Text style={styles.text}>Gestion du compte</Text>
+            </View>
+            <TouchableOpacity style={styles.menu1} activeOpacity={0.8}><Text style={styles.text3} >Modifier ses informations</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.menu1} activeOpacity={0.8}><Text style={styles.text3} >Gestion des moyens de paiements</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.menu1} activeOpacity={0.8}><Text style={styles.text3} >Gestion des notifications</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.menu1} activeOpacity={0.8}><Text style={styles.text3} >Réglages</Text></TouchableOpacity>
+         </View>
+            <View style={styles.endMenu}>
+            <TouchableOpacity style={styles.button} activeOpacity={0.8}><Text style={styles.text2} >retour</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.button} activeOpacity={0.8}><Text style={styles.text2}>Valider</Text></TouchableOpacity>
+            </View>
+        </View>
         </View>
     )
 }
@@ -15,6 +29,71 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "green",
-    }
-})
+        backgroundColor: "white",
+        paddingTop: StatusBar.currentHeight+40,
+        paddingBottom: 20,
+        
+    },
+    gestion: {
+        backgroundColor: 'white',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '106%',
+    },
+menu: {
+    width: '100%',
+    backgroundColor: "#ffffff",
+    borderRadius: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+},
+title: {
+    height: '30%',
+},
+endMenu: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    width: '90%',
+},
+button: {
+    padding: 15,
+    width: '45%',
+    backgroundColor: "#1F6F78",
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 20,
+},
+menu1: {
+    flexDirection: 'row',
+    width: '90%',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    fontSize: 20,
+    marginVertical: 5,
+    borderRadius: 5,
+    backgroundColor: "#33BBAA",
+    alignItems: "center",
+    justifyContent: "center",
+}, 
+text2: {
+    color: 'white',
+
+},
+text: {
+fontSize: 30,
+alignItems: "center",
+justifyContent: "center",
+fontWeight: "bold", 
+paddingTop: 60,
+},
+text3: {
+    width: '90%',
+    alignItems: "center",
+    justifyContent: "center",
+    
+}
+});
