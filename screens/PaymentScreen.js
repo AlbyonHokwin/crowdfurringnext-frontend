b76af09ffb4a7d284e25381f4117043ac4be45c2
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-16 16:31:26 +0100

    add functionnality to add a card and display added cards - Need to fetch paymentmethod

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import {
     StyleSheet,
     Text,
@@ -11,15 +12,27 @@ import {
     Dimensions,
     Modal,
     SafeAreaView,
+    KeyboardAvoidingView,
 } from 'react-native';
 import AddCard from '../components/AddCard';
+import CreditCard from '../components/CreditCard';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import * as colors from '../styles/colors';
 
-const BACKEND_URL = 'http://192.168.143.89:3000';
+const BACKEND_URL = 'http://192.168.10.171:3000';
 
 const PaymentScreen = ({ route, navigation }) => {
+    // const user = useSelector(state => state.user.value);
+    const user = { token: 'fzealiujeqnejbnlz1367I4njliH' };
+
     const pot = route.params.pot;
+    const [paymentNameError, setPaymentNameError] = useState(false);
+    const [cardNumberError, setCardNumberError] = useState(false);
+    const [securityCodeError, setSecurityCodeError] = useState(false);
+    const [ownerError, setOwnerError] = useState(false);
+    const [dateError, setDateError] = useState(false);
+    const [addNewCard, setAddNewCard] = useState(true);
+    const [paymentMethods, setPaymentMethods] = useState([]);
 
     const photos = pot.pictures.map((photo, i) => {
         return (
@@ -29,39 +42,128 @@ const PaymentScreen = ({ route, navigation }) => {
         );
     });
 
-    const paymentMethods = [];
+    const addCard = async card => {
+        let isValid = true;
+
+        if ((card.cardNumber.toString().match(/\d/g) || []).length !== 16) {
+            isValid = false;
+            setCardNumberError(true);
+        } else setCardNumberError(false);
+
+        if ((card.securityCode.match(/\d/g) || []).length !== 3) {
+            isValid = false;
+            setSecurityCodeError(true);
+        } else setSecurityCodeError(false);
+
+        if (!card.owner || /\d/.test(card.owner)) {
+            isValid = false;
+            setOwnerError(true);
+        } else setOwnerError(false);
+
+        if (!/\d{2}\/\d{2}/.test(card.date)) {
+            isValid = false;
+            setDateError(true);
+        } else setDateError(false);
+
+        if (!card.paymentName && user.token) {
+            isValid = false;
+            setPaymentNameError(true);
+        } else setPaymentNameError(false);
+
+        if (!isValid) return;
+
+        const newCard = {
+            paymentName: 'Carte rajoutée',
+            number: card.cardNumber,
+            expirationDate: card.date,
+            securityCode: card.securityCode,
+            nameOnCard: card.owner,
+        };
+
+        if (!user.token) {
+            setAddNewCard(false);
+            setPaymentMethods([...paymentMethods, newCard]);
+        } else {
+            newCard.paymentName = card.paymentName;
+
+            const response = await fetch(`${BACKEND_URL}/users/addpayment`, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + user.token,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ ...newCard }),
+            });
+            const data = await response.json();
+
+            if (data.result) {
+                setAddNewCard(false);
+                setPaymentMethods([...paymentMethods, newCard]);
+            }
+        }
+    };
+
+    const closeAddCard = () => {
+        setAddNewCard(false);
+    }
+
+    const creditCards = paymentMethods.map((card, i) => {
+        return (
+            <CreditCard key={i} isConnected={!!user.token} {...card} />
+        );
+    });
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.scrollContainer} >
-                <View style={styles.header}>
-                    <Text style={styles.name}>{pot.animalName}</Text>
-                    <Text style={styles.city}>{pot.user.address.city}</Text>
-                    <Text style={styles.amounts}>{pot.currentAmount}€ / {pot.targetAmount}€</Text>
-                </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : undefined}
+        >
+            <SafeAreaView style={styles.container}>
 
-                <View style={styles.photos}>
-                    <ScrollView horizontal={true} style={styles.photosScroll}>
-                        {photos}
+                <View style={styles.content} >
+                    <View style={styles.header}>
+                        <Text style={styles.name}>{pot.animalName}</Text>
+                        <Text style={styles.city}>{pot.user.address.city}</Text>
+                        <Text style={styles.amounts}>{pot.currentAmount}€ / {pot.targetAmount}€</Text>
+                    </View>
+
+                    <View style={styles.photos}>
+                        <ScrollView horizontal={true} style={styles.photosScroll}>
+                            {photos}
+                        </ScrollView>
+                    </View>
+
+                    <ScrollView style={styles.paymentMethodsContainer}>
+                        {creditCards}
+                        {addNewCard && <AddCard
+                            onPressFunction={addCard}
+                            onCloseFunction={closeAddCard}
+                            isConnected={!!user.token}
+                            paymentNameError={paymentNameError}
+                            cardNumberError={cardNumberError}
+                            securityCodeError={securityCodeError}
+                            ownerError={ownerError}
+                            dateError={dateError}
+                        />}
+                        {!addNewCard &&
+                            <TouchableOpacity onPress={() => setAddNewCard(true)} style={styles.button} activeOpacity={0.8}>
+                                <Text style={styles.textBackButton}>Rajouter une carte</Text>
+                            </TouchableOpacity>
+                        }
                     </ScrollView>
                 </View>
 
-                <View style={styles.paymentMethodsContainer}>
-                    {paymentMethods}
-                    <AddCard />
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button} activeOpacity={0.8}>
+                        <Text style={styles.textBackButton}>Retour</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => console.log('next')} style={styles.button} activeOpacity={0.8}>
+                        <Text style={styles.textGiveButton}>Vérification</Text>
+                    </TouchableOpacity>
                 </View>
-            </View>
 
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.button} activeOpacity={0.8}>
-                    <Text style={styles.textBackButton}>Retour</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => console.log('next')} style={styles.button} activeOpacity={0.8}>
-                    <Text style={styles.textGiveButton}>Vérification</Text>
-                </TouchableOpacity>
-            </View>
-
-        </SafeAreaView>
+            </SafeAreaView>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -72,9 +174,10 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: StatusBar.currentHeight + 5,
         justifyContent: 'space-between',
+        backgroundColor: colors.background,
     },
-    scrollContainer: {
-        justifyContent: 'flex-start',
+    content: {
+        justifyContent: 'flex-end',
         alignItems: 'center',
     },
     header: {
@@ -110,16 +213,7 @@ const styles = StyleSheet.create({
     },
     paymentMethodsContainer: {
         width: '80%',
-        height: Dimensions.get('screen').height / 3,
         marginVertical: 20,
-    },
-    scrollDescription: {},
-    description: {
-        fontSize: 16,
-        textAlign: 'justify',
-    },
-    voidContainer: {
-        height: 100,
     },
     buttonsContainer: {
         // position: 'absolute',
