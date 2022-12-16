c40660408b59b12de0fae75f3414932b2be240ca
yiengo <yieng.hu@gmail.com>
2022-12-16 17:25:20 +0100

    tentativePut

--- screens/PotsScreen.js
+++ screens/PotsScreen.js
@@ -1,16 +1,16 @@
 import { View, Text, StyleSheet } from "react-native";
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import ModifyInfoScreen from "./ModifyInfoScreen";
+
 
 
 export default function PotsScreen() {
     return (
-          <ModifyInfoScreen/>
+          
         
-        // <View style={styles.container}>  
-        //     <Text>POTS</Text>
-        // </View>
+        <View style={styles.container}>  
+            <Text>POTS</Text>
+        </View>
     )
 }
 
