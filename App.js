8aed7c00b5898eca1dda5db2cd62d82c16e8be06
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-18 21:45:11 +0100

    resolve conflicts

--- App.js
+++ App.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import * as colors from "./styles/colors";
-
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import user from "./reducers/user";
-
-import FontAwesome from "react-native-vector-icons/FontAwesome5";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import HomeScreen from "./screens/HomeScreen";
-import MessageScreen from "./screens/MessageScreen";
-import PotsScreen from "./screens/PotsScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import CreatePotScreen from "./screens/CreatePotScreen";
-=======
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -27,7 +7,6 @@ import user from './reducers/user';
 
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './components/TabNavigator';
->>>>>>> b76af09ffb4a7d284e25381f4117043ac4be45c2
 
 const store = configureStore({
   reducer: { user },
@@ -37,33 +16,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-<<<<<<< HEAD
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName = "";
-
-              if (route.name === "Home") iconName = "paw";
-              if (route.name === "CreatePotScreen") iconName = "envelope";
-              if (route.name === "Pots") iconName = "cat";
-              if (route.name === "Profile") iconName = "user";
-
-              return <FontAwesome name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: colors.accent,
-            tabBarInactiveTintColor: colors.shade,
-            tabBarStyle: { backgroundColor: colors.primary },
-            headerShown: false,
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="CreatePotScreen" component={CreatePotScreen} />
-          <Tab.Screen name="Pots" component={PotsScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-=======
         <TabNavigator />
->>>>>>> b76af09ffb4a7d284e25381f4117043ac4be45c2
       </NavigationContainer>
     </Provider>
   );
--- styles/colors.js
+++ styles/colors.js
@@ -1,10 +1,7 @@
 export const background = "white";
 export const backgroundModal = "rgba(100, 100, 100, 0.7)";
-<<<<<<< HEAD
-=======
 export const backgroundError = "rgba(255, 0, 0, 0.6)";
 export const borderError = 'red';
->>>>>>> b76af09ffb4a7d284e25381f4117043ac4be45c2
 export const light = "white";
 export const dark = "#434449";
 export const primary = "#1F6F78";
