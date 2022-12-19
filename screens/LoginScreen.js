70c1a9a92570315f2be9ef25c374bcc0dbed3ef8
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-19 18:21:03 +0100

    change style of login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity, StatusBar } from "
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { login } from "../reducers/user";
+import * as colors from '../styles/colors';
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -10,7 +11,7 @@ const BACKEND_URL = 'http://192.168.10.140:3000';
 export default function LoginScreen({ navigation }) {
     const dispatch = useDispatch();
 
-    const [signInEmail, setSignInEmail] = useState("");
+    const [email, setEmail] = useState("");
     const [emailError, setEmailError] = useState(false);
     const [signInPassword, setSignInPassword] = useState("");
     const [passwordError, setPasswordError] = useState(false);
@@ -18,21 +19,21 @@ export default function LoginScreen({ navigation }) {
     const handleConnection = () => {
         let signinOk = true;
 
-        if (!EMAIL_REGEX.test(signInEmail)) { setEmailError(true); signinOk = false }
-        if (signInPassword.trim().length === 0) { setPasswordError(true); signinOk = false }
+        if (!EMAIL_REGEX.test(email)) { setEmailError(true); signinOk = false }
+        if (!signInPassword.trim()) { setPasswordError(true); signinOk = false }
 
         if (signinOk) {
 
             fetch(`${BACKEND_URL}/users/signin`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ email: signInEmail, password: signInPassword })
+                body: JSON.stringify({ email, password: signInPassword })
             })
                 .then(response => response.json())
                 .then(data => {
                     if (data.result) {
                         dispatch(login({ token: data.token, email: data.email }));
-                        setSignInEmail("");
+                        setEmail("");
                         setSignInPassword("");
                         navigation.goBack();
                     }
@@ -42,24 +43,39 @@ export default function LoginScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <View>
-                <Text>Nouvel utilisateur ? Créez votre compte !</Text>
+            <View style={styles.signUpContainer}>
+                <Text style={styles.text}>Nouvel utilisateur ? Créez votre compte !</Text>
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignUp')}>
-                    <Text style={styles.text}>Créer un compte</Text>
+                    <Text style={styles.textButton}>Créer un compte</Text>
                 </TouchableOpacity>
             </View>
-            <View style={styles.inputContainer}>
-                <Text>Vous avez déjà un compte ? Connectez-vous !</Text>
-                <TextInput style={styles.input} placeholder="Adresse E-mail" onChangeText={value => setSignInEmail(value)} value={signInEmail} />
-                <TextInput style={styles.input} secureTextEntry={true} placeholder="Mot de passe" onChangeText={value => setSignInPassword(value)} value={signInPassword} />
+
+            <View style={styles.divider} />
+
+            <View style={styles.loginContainer}>
+                <Text style={styles.text}>Vous avez déjà un compte ? Connectez-vous !</Text>
+                <TextInput
+                    style={[styles.input, emailError && styles.error]}
+                    placeholderTextColor={emailError ? colors.light : undefined}
+                    placeholder="Adresse E-mail"
+                    onChangeText={value => setEmail(value)}
+                    value={email}
+                />
+                <TextInput
+                    style={[styles.input, passwordError && styles.error]}
+                    placeholderTextColor={passwordError ? colors.light : undefined}
+                    secureTextEntry={true} placeholder="Mot de passe"
+                    onChangeText={value => setSignInPassword(value)}
+                    value={signInPassword}
+                />
                 <TouchableOpacity style={styles.button} onPress={() => handleConnection()}>
-                    <Text style={styles.text}>Se connecter</Text>
+                    <Text style={styles.textButton}>Se connecter</Text>
                 </TouchableOpacity>
             </View>
-            <View style={styles.lineContainer}>
+            {/* <View style={styles.lineContainer}>
                 <View style={styles.line}></View>
                 <Text style={styles.textLine}>OU</Text>
-            </View>
+            </View> */}
         </View>
     )
 }
@@ -67,31 +83,71 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
         alignItems: "center",
-        backgroundColor: "orange",
-        paddingTop: StatusBar.currentHeight + 20,
+        backgroundColor: colors.background,
+        paddingTop: StatusBar.currentHeight + 10,
+    },
+    signUpContainer: {
+        width: '70%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    loginContainer: {
+        width: '70%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        width: '90%',
+        backgroundColor: colors.light,
+        padding: 10,
+        borderRadius: 10,
+        fontSize: 20,
+        borderWidth: 1,
+        borderColor: colors.shade,
+        color: colors.dark,
+        marginVertical: 5,
+    },
+    error: {
+        color: colors.light,
+        backgroundColor: colors.backgroundError,
+        borderColor: colors.borderError,
+    },
+    divider: {
+        width: '80%',
+        borderBottomWidth: 1,
+        borderColor: colors.dark,
+        marginVertical: 30,
     },
     button: {
         alignItems: 'center',
+        backgroundColor: colors.primary,
+        borderRadius: 10,
+        padding: 10,
         justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        marginTop: 5,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'black',
+        alignItems: 'center',
+        shadowColor: colors.accent,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        marginVertical: 5,
+        width: '70%',
+    },
+    textButton: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: colors.light,
     },
     text: {
         fontSize: 16,
-        lineHeight: 21,
         fontWeight: 'bold',
-        letterSpacing: 0.25,
-        color: 'white',
-    },
-    input: {
-        backgroundColor: "#ffffff",
-        borderRadius: 5,
-        marginVertical: 5,
+        color: colors.dark,
     },
     lineContainer: {
         width: '100%',
