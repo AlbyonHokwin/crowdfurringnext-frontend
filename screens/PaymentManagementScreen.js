32ef699cfcd19c95a613c481f786ec2c3e558fe0
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-20 16:55:50 +0100

    create page to manage payment method of user

---
+++ screens/PaymentManagementScreen.js
@@ -0,0 +1,19 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import React from 'react';
+import * as colors from "../styles/colors";
+
+const PaymentManagementScreen = () => {
+    return (
+        <View>
+            <Text>PaymentManagementScreen</Text>
+        </View>
+    );
+};
+
+export default PaymentManagementScreen;
+
+const styles = StyleSheet.create({});
