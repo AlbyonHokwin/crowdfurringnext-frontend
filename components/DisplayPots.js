91d453cfcc20ffe18ddd0772c5d6633599470724
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-20 16:29:00 +0100

    change style

--- components/DisplayPots.js
+++ components/DisplayPots.js
@@ -21,35 +21,26 @@ export default function DisplayPots(
     return (
       <View
         key={i}
-        style={{
-          width: "90%",
-          height: 120,
-          borderWidth: 1,
-          borderRadius: 10,
-          margin: 10,
-          flexDirection: "row",
-        }}
+        style={styles.potContainer}
       >
-        <View style={{ width: "30%", height: "100%", marginRight: 10 }}>
+        <View style={{ width: "30%", height: "100%" }}>
           <Image
             source={{
               uri: pot.pictures[0],
               width: "100%",
               height: "100%",
             }}
-            style={{ borderRadius: 10 }}
+            style={{ borderTopLeftRadius: 9, borderBottomLeftRadius: 9 }}
           />
         </View>
-        <View style={{ width: "60%", alignItems: "center" }}>
-          <Text>{pot.animalName}</Text>
-          <Text>
-            {pot.currentAmount} / {pot.targetAmount}€
-          </Text>
+        <View style={{ width: "60%", justifyContent: "space-around", alignItems: "center" }}>
+          <Text style={styles.animalName}>{pot.animalName}</Text>
+          <Text style={styles.amount}>{pot.currentAmount} / {pot.targetAmount}€</Text>
           <View
             style={{
-              width: "100%",
+              width: "90%",
               borderWidth: 1,
-              marginTop: 10,
+              borderColor: colors.shade,
               height: 10,
               borderRadius: 10,
               flexDirection: "row",
@@ -60,6 +51,7 @@ export default function DisplayPots(
                 ([StyleSheet.absoluteFill],
                 {
                   backgroundColor: colors.secondary,
+                  borderRadius: 10,
                   width: `${Math.min(1, (pot.currentAmount / pot.targetAmount)) * 100}%`,
                 })
               }
@@ -71,40 +63,25 @@ export default function DisplayPots(
               width: "100%",
               flexDirection: "row",
               justifyContent: `${boolean ? "space-between" : "center"}`,
+              paddingBottom: 5,
             }}
           >
             <TouchableOpacity
-              style={{
-                backgroundColor: colors.secondary,
-                width: "30%",
-                height: 40,
-                borderRadius: 10,
-                alignItems: "center",
-                justifyContent: "center",
-                margin: 20,
-              }}
+              style={styles.button}
               onPress={() => navigation.navigate("Pot", { slug })}
             >
-              <Text style={{ color: "white" }}>Voir</Text>
+              <Text style={styles.textButton}>Voir</Text>
             </TouchableOpacity>
             {boolean && (
               <TouchableOpacity
-                style={{
-                  backgroundColor: colors.secondary,
-                  width: "30%",
-                  height: 40,
-                  borderRadius: 10,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  margin: 20,
-                }}
+                style={styles.button}
                 onPress={() => {
                   setId(pot._id);
                   setModalVisible(true);
                   setDouble(true);
                 }}
               >
-                <Text style={{ color: "white" }}>Clôturer</Text>
+                <Text style={styles.textButton}>Clôturer</Text>
               </TouchableOpacity>
             )}
           </View>
@@ -113,3 +90,55 @@ export default function DisplayPots(
     );
   });
 }
+
+const styles = StyleSheet.create({
+  potContainer: {
+    width: "90%",
+    height: 120,
+    margin: 10,
+    flexDirection: "row",
+    backgroundColor: colors.light,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: colors.shade,
+    shadowColor: colors.accent,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  animalName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: colors.dark,
+  },
+  amount: {
+    fontSize: 16,
+    color: colors.dark,
+  },
+  button: {
+    width: "30%",
+    marginHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: colors.secondary,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: colors.accent,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: colors.light,
+  },
+});
