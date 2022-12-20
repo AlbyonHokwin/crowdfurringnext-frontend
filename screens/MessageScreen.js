56036dc11cab0f4cbccc2224cd5fd06fce142e4f
Langbien <pierre.langbien@gmail.com>
2022-12-20 14:23:27 +0100

    télécharger photo de profil terminé

--- screens/MessageScreen.js
+++ screens/MessageScreen.js
@@ -1,15 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import SignUpScreen from "./SignUpScreen";
-
 
 export default function MessageScreen() {
     return (
-        // // <View style={styles.container}>  
-        //     {/* <Text>MESSAGE</Text> */}
-        // {/* </View> */}
-        <SignUpScreen />
+        <View style={styles.container}>  
+            <Text>MESSAGE</Text>
+        </View>
     )
 }
 
