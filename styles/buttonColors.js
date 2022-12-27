0a6af79dc47206dfb0ed3a64a2e42d11ac6ecfed
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-27 12:23:17 +0100

    add buttonColors

---
+++ styles/buttonColors.js
@@ -0,0 +1,16 @@
+import { colors } from './colors';
+
+const buttonColor = {
+    primary: {
+        backgroundColor: colors.primary,
+        color: colors.light,
+    },
+    secondary: {
+        backgroundColor: colors.secondary,
+        color: colors.light,
+    },
+    tertiary: {
+        backgroundColor: colors.tertiary,
+        color: colors.dark,
+    },
+};
