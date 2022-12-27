c2828f3eca99e68e251eb644bd78ac976ef46c6a
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-27 12:37:07 +0100

    change calculation with fontScale

--- styles/fonts.js
+++ styles/fonts.js
@@ -3,11 +3,11 @@ import { Dimensions } from 'react-native';
 const { fontScale } = Dimensions.get('screen');
 
 const fonts = {};
-fonts.baseSmall = { normal: { fontSize: fontScale * 16 } };
-fonts.base = { normal: { fontSize: fontScale * 20 } };
-fonts.baseBig = { normal: { fontSize: fontScale * 24 } };
-fonts.title = { normal: { fontSize: fontScale * 30 } };
-fonts.titleBig = { normal: { fontSize: fontScale * 40 } };
+fonts.baseSmall = { normal: { fontSize: 16 / fontScale } };
+fonts.base = { normal: { fontSize: 20 / fontScale } };
+fonts.baseBig = { normal: { fontSize: 24 / fontScale } };
+fonts.title = { normal: { fontSize: 30 / fontScale } };
+fonts.titleBig = { normal: { fontSize: 40 / fontScale } };
 
 Object.keys(fonts).forEach(key => fonts[key].bold = { ...fonts[key].normal, fontWeight: 'bold' });
 
