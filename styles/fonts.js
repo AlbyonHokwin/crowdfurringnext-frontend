5c15ee659a608c2a9a9ffabae9fe852dd6e49757
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-27 12:15:50 +0100

    add fonts

--- styles/fonts.js
+++ styles/fonts.js
@@ -1,3 +1,14 @@
-export const body = {
-    fontSize: 16,
-};
+import { Dimensions } from 'react-native';
+
+const { fontScale } = Dimensions.get('screen');
+
+const fonts = {};
+fonts.baseSmall = { normal: { fontSize: fontScale * 16 } };
+fonts.base = { normal: { fontSize: fontScale * 20 } };
+fonts.baseBig = { normal: { fontSize: fontScale * 24 } };
+fonts.title = { normal: { fontSize: fontScale * 30 } };
+fonts.titleBig = { normal: { fontSize: fontScale * 40 } };
+
+Object.keys(fonts).forEach(key => fonts[key].bold = { ...fonts[key].normal, fontWeight: 'bold' });
+
+export { fonts };
