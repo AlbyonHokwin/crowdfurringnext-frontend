70c3fd4bf5a370ab69eb5536545aa039851cd9b6
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-27 15:58:18 +0100

    reduce all fontSize by 2

--- styles/fonts.js
+++ styles/fonts.js
@@ -3,10 +3,10 @@ import { Dimensions } from 'react-native';
 const { fontScale } = Dimensions.get('screen');
 
 const fonts = {};
-fonts.baseSmall = { normal: { fontSize: 16 / fontScale } };
-fonts.base = { normal: { fontSize: 20 / fontScale } };
-fonts.baseBig = { normal: { fontSize: 24 / fontScale } };
-fonts.title = { normal: { fontSize: 30 / fontScale } };
+fonts.baseSmall = { normal: { fontSize: 14 / fontScale } };
+fonts.base = { normal: { fontSize: 18 / fontScale } };
+fonts.baseBig = { normal: { fontSize: 22 / fontScale } };
+fonts.title = { normal: { fontSize: 28 / fontScale } };
 fonts.titleBig = { normal: { fontSize: 40 / fontScale } };
 
 Object.keys(fonts).forEach(key => fonts[key].bold = { ...fonts[key].normal, fontWeight: 'bold' });
