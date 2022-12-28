794ec5823cd5e9c06e38d7a7ac8697856fcf7345
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-28 17:15:33 +0100

    finish customTextInput - can handle, focus, error and next

--- components/CustomTextInput.js
+++ components/CustomTextInput.js
@@ -1,22 +1,25 @@
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 import React from 'react';
-import { useRef, useState } from 'react';
+import { useState, useRef } from 'react';
 import { colors } from '../styles/colors';
 import { fonts } from '../styles/fonts';
 
-const CustomTextInput = ({ label, width, isError, ...props }) => {
+const CustomTextInput = ({ label, width, isError, step, currentStep, maxStep, ...props }) => {
+    // const [isFocused, setIsFocused] = useState(false)
     const ref = useRef(null);
-    const [isFocused, setIsFocused] = useState(false)
+
+    const isFocused = step === currentStep;
+
+    isFocused && ref.current.focus();
 
     return (
         <View style={[{ width }, styles.container, isFocused && styles.containerFocused, isError && styles.containerError]}>
-            <Text style={isFocused ? styles.labelFocused : styles.label}>{label}</Text>
+            <Text style={[styles.label, isFocused && styles.labelFocused, isError && styles.labelError]}>{label}</Text>
             <TextInput
                 ref={ref}
-                style={[isFocused ? styles.inputFocused : styles.input]}
+                style={[styles.input, isFocused && styles.inputFocused, isError && styles.inputError]}
                 placeholderTextColor={isFocused ? `${colors.accent}55` : isError ? colors.error : colors.shade}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
+                returnKeyType={step < maxStep ? 'next' : 'done'}
                 {...props}
             />
         </View>
@@ -36,20 +39,12 @@ const styles = StyleSheet.create({
         height: 3 * fonts.baseSmall.normal.fontSize,
     },
     containerFocused: {
-        backgroundColor: colors.background,
-        padding: 10,
-        borderRadius: 10,
-        borderWidth: 1,
+        borderWidth: 2,
+        padding: 9,
         borderColor: colors.accent,
-        marginVertical: 5,
     },
     containerError: {
-        backgroundColor: colors.background,
-        padding: 10,
-        borderRadius: 10,
-        borderWidth: 1,
         borderColor: colors.borderError,
-        marginVertical: 5,
     },
     label: {
         ...fonts.baseSmall.normal,
@@ -62,24 +57,13 @@ const styles = StyleSheet.create({
         borderRadius: 1000,
     },
     labelFocused: {
-        ...fonts.baseSmall.normal,
+        top: -fonts.baseSmall.normal.fontSize / 2 - 1,
+        left: 9,
+        ...fonts.baseSmall.bold,
         color: colors.accent,
-        position: 'absolute',
-        top: -fonts.baseSmall.normal.fontSize / 2,
-        left: 10,
-        backgroundColor: colors.background,
-        paddingHorizontal: 5,
-        borderRadius: 1000,
     },
     labelError: {
-        ...fonts.baseSmall.normal,
         color: colors.error,
-        position: 'absolute',
-        top: -fonts.baseSmall.normal.fontSize / 2,
-        left: 10,
-        backgroundColor: colors.background,
-        paddingHorizontal: 5,
-        borderRadius: 1000,
     },
     input: {
         width: '100%',
@@ -87,11 +71,10 @@ const styles = StyleSheet.create({
         color: colors.dark,
     },
     inputFocused: {
-        width: '100%',
         ...fonts.base.bold,
         color: colors.accent,
     },
-    error: {
-        color: colors.light,
+    inputError: {
+        color: colors.error,
     },
 });
