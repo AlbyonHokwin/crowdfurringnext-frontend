06f33740095775cf6fc3ca46e3e1ef683249ae96
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-30 09:43:22 +0100

    add reducer to update profile picture, add picture to initial state and login reducer

--- reducers/user.js
+++ reducers/user.js
@@ -4,6 +4,7 @@ const initialState = {
   value: {
     token: "",
     email: "",
+    picture: "",
   },
 };
 
@@ -14,13 +15,18 @@ export const userSlice = createSlice({
     login: (state, action) => {
       state.value.token = action.payload.token;
       state.value.email = action.payload.email;
+      state.value.picture = action.payload.picture;
+    },
+    updatePicture: (state, action) => {
+      state.value.picture = action.payload;
     },
     logout: (state, action) => {
       state.value.token = "";
       state.value.email = "";
+      state.value.picture = "";
     },
   },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { login, updatePicture, logout } = userSlice.actions;
 export default userSlice.reducer;
